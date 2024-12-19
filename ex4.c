588a561fbf1d603a82b1e4490d1b7d5b236fe1cd
AmitRothschild1 <amitrot1234@gmail.com>
2024-12-19 16:52:32 +0200

    case 1 & 2

--- ex4.c
+++ ex4.c
@@ -6,9 +6,9 @@ Assignment:
 #include <stdio.h>
 #include <string.h>
 
-void task1RobotPaths();
-void task2HumanPyramid();
-void task3ParenthesisValidator();
+int task1RobotPaths(int,int);
+float task2HumanPyramid();
+int task3ParenthesisValidator();
 void task4QueensBattle();
 void task5CrosswordGenerator();
 
@@ -33,10 +33,44 @@ int main()
                 printf("Goodbye!\n");
                 break;
             case 1:
-                task1RobotPaths();
+                int column , row;
+                printf("Please enter the coordinates of the robot (column, row):\n");
+                scanf("%d %d",&column,&row);
+                printf("The total number of paths the robot can take to reach home is: %d\n",task1RobotPaths(column,row));
                 break;
             case 2:
-                task2HumanPyramid();
+                float arr[5][9]={0};
+                //scanf("%f", &
+                arr[4][4]=51.18;
+                // scanf("%f %f", &
+                arr[3][3]=55.90;
+                arr[3][5]=131.25;
+                //scanf("%f %f %f", &
+                arr[2][2]=69.05;
+                arr[2][4]=133.66;
+                arr[2][6]=132.82;
+                //scanf("%f %f %f %f", &
+                arr[1][1]=53.43;
+                arr[1][3]=139.61;
+                arr[1][5]=134.06;
+                arr[1][7]=121.63;
+                //scanf("%f %f %f %f %f", &
+                arr[0][0]=72.31;
+                arr[0][2]=108.62;
+                arr[0][4]=171.24;
+                arr[0][6]=151.33;
+                arr[0][8]=93.32;
+                for(int i=4;i>-1;i--)
+                {
+                    for(int j=i;j<9;j++)
+                    {
+                        if(j%2 != i%2)
+                        j++;
+                       if (j+i <= 8)
+                           printf("%.2f ",task2HumanPyramid(j,i,arr));
+                    }
+                    printf("\n");
+                }
                 break;
             case 3:
                 task3ParenthesisValidator();
@@ -60,17 +94,25 @@ int main()
     } while (task != 6);
 }
 
-void task1RobotPaths()
+int task1RobotPaths(int col ,int row)
 {
     // Todo
+    if(col == 0 && row == 0)
+        return 1;
+    if(col < 0 || row < 0)
+        return 0;
+    return task1RobotPaths(col - 1,row) + task1RobotPaths(col,row - 1);
 }
 
-void task2HumanPyramid()
+float task2HumanPyramid(int col ,int row,float arr[5][9])
 {
     // Todo
+    if(col-row < 0 || col+row > 8)
+        return 0;
+    return task2HumanPyramid(col+1,row+1,arr)/2 + task2HumanPyramid(col-1,row+1,arr)/2 + arr[row][col];
 }
 
-void task3ParenthesisValidator()
+int task3ParenthesisValidator()
 {
     // Todo
 }
